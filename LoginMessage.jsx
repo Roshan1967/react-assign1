7c01a97f108a31950a13f92bc31ea684ddcefb11
Roshan Kumar <153509225+Roshan1967@users.noreply.github.com>
2024-05-29 11:20:13 +0530

    Add files via upload

---
+++ LoginMessage.jsx
@@ -0,0 +1,10 @@
+// src/components/LoginMessage.jsx
+
+import React from "react";
+
+function LoginMessage({ isLoggedIn }) {
+
+  return <div>{isLoggedIn? "Hello,User!":"Access Denied!"}</div>;
+}
+
+export default LoginMessage;
